deebe87159078772216a12d2e9e2b2cb984af2d5
Bryan Gordon <bryanbge92@gmail.com>
2024-11-12 17:39:15 -0500

    Initialize server

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { PORT } from './config.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+// Endpoints
+app.post('/login', (req, res) => {
+  res.send('Probando Yaak')
+})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+
+app.post('/protected', (req, res) => {})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
