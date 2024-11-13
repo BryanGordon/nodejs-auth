0ca52704e1752d14beacca457333f196368dcd5c
Bryan Gordon <bryanbge92@gmail.com>
2024-11-13 17:38:51 -0500

    Add DB validations

--- user-repository.js
+++ user-repository.js
@@ -1,6 +1,27 @@
-import dbLocal from 'db-local'
+import DBLocal from 'db-local'
+
+const { Schema } = new DBLocal({ path: './db' })
+
+const User = Schema('User', {
+  _id: { type: String, requiered: true },
+  username: { type: String, requiered: true },
+  password: { type: String, requiered: true }
+})
 
 export class UserRepository {
-  static create ({ username, password }) {}
+  static create ({ username, password }) {
+    // Usar biblioteca de validaciones llamada zod
+    // Primero se usan validaciones
+    if (typeof username !== 'string') throw new Error('username must be a string')
+    if (username.length < 3) throw new Error('username must be at least 3 characters long')
+
+    if (typeof password !== 'string') throw new Error('password must be a string ')
+    if (password.length < 5) throw new Error('password must be have more than 4 characters')
+
+    // Asegurarse que le username no existe
+    const user = User.findOne({ username })
+    if (user) throw new Error('username alredy exist')
+  }
+
   static login ({ username, password }) {}
 }
