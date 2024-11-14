36ae3875fe9c5c979312aae420095d24b73bf10d
Bryan Gordon <bryanbge92@gmail.com>
2024-11-14 17:32:24 -0500

    Update endpoints

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello world')
@@ -11,7 +13,17 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
   res.send('Probando Yaak')
 })
-app.post('/register', (req, res) => {})
+app.post('/register', (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const id = UserRepository.create({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
+
 app.post('/logout', (req, res) => {})
 
 app.post('/protected', (req, res) => {})
