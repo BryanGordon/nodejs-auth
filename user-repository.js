ca675bfb5c74cd19b3a3e20f14b607544b961468
Bryan Gordon <bryanbge92@gmail.com>
2024-11-15 17:46:43 -0500

    Add login logic

--- user-repository.js
+++ user-repository.js
@@ -1,5 +1,8 @@
 import DBLocal from 'db-local'
 import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+
+import { SALT_ROUNDS } from './config.js'
 
 const { Schema } = new DBLocal({ path: './db' })
 
@@ -17,22 +20,34 @@ export class UserRepository {
     if (username.length < 3) throw new Error('username must be at least 3 characters long')
 
     if (typeof password !== 'string') throw new Error('password must be a string ')
-    if (password.length < 5) throw new Error('password must be have more than 4 characters')
+    if (password.length < 5) throw new Error('password must be have more than 5 characters')
 
     // Asegurarse que le username no existe
     const user = User.findOne({ username })
     if (user) throw new Error('username alredy exist')
 
     const id = crypto.randomUUID()
+    // Se ha una encriptacion de la contraseña
+    const hashedPassword = bcrypt.hashSync(password, SALT_ROUNDS)
 
     User.create({
       _id: id,
       username,
-      password
+      password: hashedPassword
     }).save()
 
     return id
   }
 
-  static login ({ username, password }) {}
+  static login ({ username, password }) {
+    // Crear validaciones
+    // Hacer un metodo para las validaciones
+    const user = User.findOne({ username })
+    if (!user) throw new Error('Username does not exist')
+
+    const isValid = bcrypt.compareSync(password, user.password)
+    if (!isValid) throw new Error('Password is invalid')
+
+    return user
+  }
 }
