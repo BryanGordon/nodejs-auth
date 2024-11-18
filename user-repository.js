b045fb00f3cac06fc3b5ac7dda68b8c5c3cc2573
Bryan Gordon <bryanbge92@gmail.com>
2024-11-18 17:50:01 -0500

    Create validations

--- user-repository.js
+++ user-repository.js
@@ -12,15 +12,24 @@ const User = Schema('User', {
   password: { type: String, requiered: true }
 })
 
+class Validation {
+  static username (username) {
+    if (typeof username !== 'string') throw new Error('username must be a string')
+    if (username.length < 3) throw new Error('username must be at least 3 characters long')
+  }
+
+  static password (password) {
+    if (typeof password !== 'string') throw new Error('password must be a string ')
+    if (password.length < 5) throw new Error('password must be have more than 5 characters')
+  }
+}
+
 export class UserRepository {
   static create ({ username, password }) {
     // Usar biblioteca de validaciones llamada zod
     // Primero se usan validaciones
-    if (typeof username !== 'string') throw new Error('username must be a string')
-    if (username.length < 3) throw new Error('username must be at least 3 characters long')
-
-    if (typeof password !== 'string') throw new Error('password must be a string ')
-    if (password.length < 5) throw new Error('password must be have more than 5 characters')
+    Validation.username(username)
+    Validation.password(password)
 
     // Asegurarse que le username no existe
     const user = User.findOne({ username })
@@ -40,14 +49,17 @@ export class UserRepository {
   }
 
   static login ({ username, password }) {
-    // Crear validaciones
-    // Hacer un metodo para las validaciones
+    Validation.username(username)
+    Validation.password(password)
+
     const user = User.findOne({ username })
     if (!user) throw new Error('Username does not exist')
 
     const isValid = bcrypt.compareSync(password, user.password)
     if (!isValid) throw new Error('Password is invalid')
 
-    return user
+    const { password: privateUser, ...publicUser } = user
+
+    return publicUser
   }
 }
