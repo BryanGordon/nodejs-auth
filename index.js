8d2cb0b2c4703f92b00b4cd0d4ae4e032f435561
Bryan Gordon <bryanbge92@gmail.com>
2024-11-20 18:12:58 -0500

    Add jwt functionality

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = data
+  } catch (e) {}
+
+  next()
+})
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 // Endpoints
@@ -17,7 +33,16 @@ app.post('/login', (req, res) => {
 
   try {
     const user = UserRepository.login({ username, password })
-    res.send({ user })
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, { expiresIn: '1h' })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60
+
+      })
+      .send({ user, token })
   } catch (e) {
     res.status(401).send(e.message)
   }
@@ -34,9 +59,19 @@ app.post('/register', (req, res) => {
   }
 })
 
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('access_token')
+    .json({ message: 'Logout successfully' })
+})
 
-app.post('/protected', (req, res) => {})
+app.get('/protected', (req, res) => {
+  const { user } = req.session
+
+  if (!user) return res.status(403).send('Access denied')
+
+  res.render('protected', user)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
